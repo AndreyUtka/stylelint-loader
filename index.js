8ea266c3affb3934a50efe070c5303e3909fa078
Andrey Michaluik <andreyutka@gmail.com>
2016-01-30 23:32:23 +0200

    fix: prepare index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ var assign = require('object-assign'),
 var defaultOptions = {
     configFile: './.stylelint.config.js',
     displayOutput: true,
-    ignoreCache: false
+    ignoreCache: false,
+    webpackWarnings: true,
+    webpackErrors: true,
+    files: null
 };
 
 var lintedFiles = [];
@@ -32,12 +35,42 @@ function linter(content, options, context, callback) {
         }
     }
 
-    // Display Path is what we show to the user
     var filePath = context.resourcePath;
     if (filePath.indexOf(__dirname) === 0) {
         filePath = filePath.replace(__dirname, '.');
     }
 
+    if (options.files) {
+        lintFiles(processFile, filePath, content, options, context, callback);
+    } else {
+        lintFile(processFile, filePath, content, options, context, callback);
+    }
+}
+
+function lintFile(processFile, filePath, content, options, context, callback) {
+    console.log(options)
+    var lintOptions = assign({}, options, {
+        code: fs.readFileSync(context.resourcePath, { encoding: 'utf-8' }),
+        syntax: path.extname(filePath).replace('.', ''),
+        formatter: 'json'
+    });
+    if (processFile) {
+        stylelint.lint(lintOptions)
+            .then(data => {
+                console.log(data.results)
+                return data.results[0];
+            })
+            .then(result => processResult(result, content, options, context, callback, filePath))
+            .catch(error => {
+                callback(error);
+            });
+        callback(null, content);
+    } else if (callback) {
+        callback(null, content);
+    }
+}
+
+function lintFiles(processFile, filePath, content, options, context, callback) {
     var lintOptions = assign({}, options, {
         syntax: path.extname(filePath).replace('.', ''),
         formatter: 'json'
@@ -47,36 +80,51 @@ function linter(content, options, context, callback) {
         stylelint.lint(lintOptions)
             .then(data => {
                 data.results.forEach(result => {
-                    if (options.displayOutput && result.warnings.length > 0) {
-                        console.log(chalk.blue.underline.bold(result.source));
-                    }
-                    result.warnings.forEach(warning => {
-                        var position = `${warning.line}:${warning.column}`;
-                        if (warning.severity === 'warning') {
-                            if (options.displayOutput) {
-                                console.log(chalk.yellow(`${position} ${warning.text}`));
-                            }
-                            if (options.webpackWarnings) {
-                                context.emitWarning(`${position} ${warning.text}`);
-                            }
-                        } else if (warning.severity === 'error') {
-                            if (options.displayOutput) {
-                                console.log(chalk.red(`${position} ${warning.text}`));
-                            }
-                            if (options.webpackErrors) {
-                                context.emitError(`${position} ${warning.text}`);
-                            }
-                        }
-                    });
-                    if (options.displayOutput && result.warnings.length > 0) {
-                        console.log('');
-                    }
+                    processResult(result, content, options, context, callback);
                 });
-                console.log(chalk.blue('\n Come on, fix you sass nigga (Last check: ' + new Date() + ')'));
                 callback(null, content);
+            })
+            .catch(error => {
+                callback(error);
             });
     } else if (callback) {
         callback(null, content);
+    }
+}
+
+function processResult(result, content, options, context, callback, filePath) {
+    if (options.displayOutput && result.warnings.length > 0) {
+        var path = filePath ? filePath : result.source;
+        console.log(chalk.blue.underline.bold(path));
+    }
+    result.warnings.forEach(warning => {
+        var position = `${warning.line}:${warning.column}`;
+        if (!warning.severity) {
+            if (options.displayOutput) {
+                console.log(chalk.cyan(`${warning.text}`));
+            }
+            if (options.webpackErrors) {
+                context.emitError(`${warning.text}`);
+            }
+        }
+        else if (warning.severity === 'warning') {
+            if (options.displayOutput) {
+                console.log(chalk.yellow(`${position} ${warning.text}`));
+            }
+            if (options.webpackWarnings) {
+                context.emitWarning(`${position} ${warning.text}`);
+            }
+        } else if (warning.severity === 'error') {
+            if (options.displayOutput) {
+                console.log(chalk.red(`${position} ${warning.text}`));
+            }
+            if (options.webpackErrors) {
+                context.emitError(`${position} ${warning.text}`);
+            }
+        }
+    });
+    if (options.displayOutput && result.warnings.length > 0) {
+        console.log('');
     }
 }
 
